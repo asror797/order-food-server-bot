833af99b99bc53a2ffc2c01c807b8b2688fac5f3
Asrorbek Shahobiddinov <asrorshahobiddinov509@gmail.com>
2023-10-04 11:48:34 +0500

    cleanup

--- src/database/database.ts
+++ src/database/database.ts
@@ -2,7 +2,7 @@ import { ConnectOptions } from "mongoose";
 
 /* constants */
 const PATH = 'mongodb://localhost:27017/foodbot'
-const PROD_PATH = 'mongodb+srv://***:*****@db-mongodb-fra1-82679-5f731662.mongo.ondigitalocean.com/botdb?tls=true&authSource=admin'
+const PROD_PATH = 'mongodb+srv://doadmin:U8N1CoMQ57W49f06@db-mongodb-fra1-82679-5f731662.mongo.ondigitalocean.com/botdb?tls=true&authSource=admin'
 
 interface DB_CONNECTION {
   url: string;
